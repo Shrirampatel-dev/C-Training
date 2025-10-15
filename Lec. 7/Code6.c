d4ec51f1764f8358f63287e09dd287ccaf031bfe
Shriram Patel <patelshriram777@gmail.com>
2025-10-15 21:28:45 +0530

    Add files via upload

---
+++ Lec. 7/Code6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=7;i++){
+        for(int j=1;j<=7;j++){
+            printf("*");
+            if (i==j);
+        }
+        printf("\n");
+    }
+return 0;
+}
